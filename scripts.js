62555f68bd31dc56827aacf9f5ce7fec1b1511a4
Egiemilhas <egiemilhas@gmail.com>
2023-12-15 22:59:19 +0700

    Deskripsi perubahan Anda

--- scripts.js
+++ scripts.js
@@ -1,19 +1,29 @@
-const content = document.getElementById('content');
-const backgroundMusic = document.getElementById('backgroundMusic');
+document.addEventListener('DOMContentLoaded', function () {
+  const content = document.getElementById('content');
+  const surpriseButton = document.getElementById('surpriseButton');
 
-// Gunakan innerHTML untuk mengganti pesan cinta
-content.innerHTML = `
-    <h1>Surat Cinta Untukmu</h1>
-    <p>
-        Sayang, terimakasih telah menjadi bagian dalam hidupku.
-        <br>
-        Setiap momen bersamamu adalah anugerah.
-        <br>
-        Meski nethingmu berlebih <br>
-        Meski insecuhdafsddgau berlebih <br>
-        ketahuilah Aku mencintaimu lebih dari apapun.
-    </p>
-`;
+  // Tambahkan event listener untuk memuat pesan cinta yang berbeda saat halaman dimuat
+  content.addEventListener('mouseover', function () {
+    content.innerHTML = `
+            <h1>Surat Cinta Aulia</h1>
+            <p>
+                Sayang, terimakasih telah menjadi bagian dalam hidupku.
+                <br>
+                Setiap momen bersamamu adalah anugerah.
+                <br>
+                Meski nethingmu berlebih <br>
+                Meski insecuremu berlebih <br>
+                Ketahuilah Aku menyukainya <br> dan mencintaimu lebih dari apapun.
+            </p>
+        `;
+  });
 
-// Ganti 'your_music.mp3' dengan nama file musik Anda
-backgroundMusic.src = 'your_music.mp3';
+  // Tambahkan event listener untuk menampilkan kejutan saat tombol ditekan
+  surpriseButton.addEventListener('click', function () {
+    showSurprise();
+  });
+
+  function showSurprise() {
+    alert('🎉 Selamat, kamu mendapat kejutan!');
+  }
+});
